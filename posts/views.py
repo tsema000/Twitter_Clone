3a0c803496e628ad5f56db4e73a232852c9f8f99
Tilahun <Tilahun362@gmail.com>
2022-03-03 23:58:58 -0500

    adding the files of twitter clone

---
+++ posts/views.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.forms.fields import ImageField
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import PostForm
+from .models import Post
+
+
+# Create your views here.
+def index(request):
+    # if the method is POST
+    if request.method =='POST':
+        form = PostForm(request.POST, request.FILES)
+        
+        # if the form is valid
+        if form.is_valid():
+        # Yes, Save
+            form.save()
+        
+        # Redirect to Home
+            return HttpResponseRedirect('/')
+
+            
+        else:
+            # No, Show errors
+            return HttpResponseRedirect(form.errors.as_json())
+
+    # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_at')[:20]
+    
+
+    # Show
+    return render(request, 'posts.html', {'posts': posts})
+
+def likes(request, id):
+    print(id)
+    likedtweet = Post.objects.get(id=id)
+    likedtweet.like_count += 1
+    likedtweet.save()
+    return HttpResponseRedirect("/")
+
+def delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+
+
+def edit(request, id):
+    if request.method == "GET":
+        posts = Post.objects.get(id=id)
+        return render(request, "edit.html", {"posts": posts})
+    if request.method == "POST":
+        editposts = Post.objects.get(id=id)
+        form = PostForm(request.POST, request.FILES, instance=editposts)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponse("not valid")
+
+
